93c10ac69b74f34cabb78cfe0f348682c7824493
Luke Doty <lukedoty03@gmail.com>
2024-07-17 00:00:26 +0200

    Quick fix

--- js/page-nav.js
+++ js/page-nav.js
@@ -20,30 +20,34 @@ const pages = {
     }
 };
 
+// create document click that watches the nav links only
 document.addEventListener("click", (event) => {
     const { target } = event;
     if (!target.matches("nav a")) return;
     event.preventDefault();
-    navigate(event);
+    route(event);
 });
 
-async function navigate(event) {
+function route(event) {
     event.preventDefault();
     window.history.pushState({}, "", event.target.href);
-    
+    pageHandler();
+};
+
+async function pageHandler() {
     const location = window.location.pathname;
     if (location.length == 0) {
         location = "/";
     }
 
-    const page = pages[location] || pages["404"];
+    const [page] = pages[location] || pages["404"];
     await pageContainer.goToPage(page.pagePath);
-    document.title = "Luke Doty | " + route.title;
-};
+    document.title = "Luke Doty | " + page.title;
+}
 
-window.onpopstate = locationHandler;
-window.route = navigate;
-locationHandler();
+window.onpopstate = pageHandler;
+window.route = route;
+pageHandler();
 
 
 
