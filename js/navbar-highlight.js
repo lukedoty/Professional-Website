1876cd1b65d59dd7d48639401ff9d65c644a2451
Luke Doty <lukedoty03@gmail.com>
2024-07-18 00:03:27 +0200

    Home from title triggers navbar. Css formatting updates

--- js/navbar-highlight.js
+++ js/navbar-highlight.js
@@ -5,12 +5,13 @@ document.addEventListener("click", function(event) {
     let navLink = event.target.closest(".nav-link");
     if (!navLink) return;
 
-    console.log(navLinks);
     for(nl of navLinks) {
         nl.classList.remove("selected");
     }
 
-    if (!navLink.matches("header nav a.nav-link")) return;
-    
-    navLink.classList.add("selected");
+    if (navLink.matches("header nav a.nav-link")) {
+        navLink.classList.add("selected");
+    }else if (navLink.matches("#website-title")) {
+        navLinks[0].classList.add("selected");
+    }
 });
