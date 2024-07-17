77b52a042e722679d70f700f03b80f241f0847b0
Luke Doty <lukedoty03@gmail.com>
2024-07-17 22:28:45 +0200

    Navbar more robust

--- js/navbar-highlight.js
+++ js/navbar-highlight.js
@@ -1,10 +1,16 @@
+const navLinks = document.querySelectorAll("header nav a.nav-link");
+document.querySelector('header nav a.nav-link[href="' + window.location.pathname + '"]').classList.add("selected");
+
 document.addEventListener("click", function(event) {
-    let navLink = event.target.closest("header nav a.nav-link");
+    let navLink = event.target.closest(".nav-link");
     if (!navLink) return;
 
-    for(nl of navLink.parentElement.children) {
+    console.log(navLinks);
+    for(nl of navLinks) {
         nl.classList.remove("selected");
     }
+
+    if (!navLink.matches("header nav a.nav-link")) return;
     
     navLink.classList.add("selected");
 });
